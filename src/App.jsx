5c155931780c6a4ac9dda4c0c504599102198a6d
drejon <drc.working@gmail.com>
2023-11-30 12:35:39 +0100

    Delete obsolete lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,67 @@
 import './App.css'
-import { Cards } from './components/Cards'
-import { Contact } from './components/Contact'
-import { Projects } from './components/Projects'
-import { experience, projects, studies, svgs } from './consts/consts'
 
-export function App() {
+import { useState, useEffect } from 'react'
 
+import { Home } from './pages/Home'
+import { Battleship } from './pages/battleship/src/Battleship'
+import { Minesweeper } from './pages/minesweeper/src/Minesweeper'
+import { TicTacToe } from './pages/tic-tac-toe/src/TicTacToe'
+
+const routes = [
+  {
+    path: '/',
+    Component: Home
+  },
+  {
+    path: '/demos/battleship',
+    Component: Battleship
+  },
+  {
+    path: '/demos/minesweeper',
+    Component: Minesweeper
+  },
+  {
+    path: '/demos/tic-tac-toe',
+    Component: TicTacToe
+  },
+]
+
+const NAVIGATION_EVENT = 'pushstate'
+
+export function navigate(href) {
+  window.history.pushState({}, '', href)
+
+  const navigationEvent = new Event(NAVIGATION_EVENT)
+  window.dispatchEvent(navigationEvent)
+}
+
+function Router ({ routes = [], defaultComponent: DefaultComponent = () => {<h1>404</h1>} }) {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect( () => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+    window.addEventListener('popstate', onLocationChange)
+
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+      window.removeEventListener('popstate', onLocationChange)
+    }
+  }, [])
+
+  const Page = routes.find(({path}) => path === currentPath)?.Component
+
+  return Page ? <Page/> : <DefaultComponent/>
+}
+
+export function App () {
+  
   return (
-    <>
-    <main className='container'>
-      <header>
-        <h1 className='name'>Diego Alejandro Rejón Clemente</h1>
-        <h2 className='description'>Web Developer</h2>
-        <Contact></Contact>
-      </header>
-
-      <Cards content={experience}></Cards>
-      <Projects content={projects}></Projects>
-      <Projects content={svgs}></Projects>
-      <Cards content={studies}></Cards>
+    <main>
+      <Router routes={routes}/>
     </main>
-    </>
   )
 }
