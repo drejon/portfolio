8ba9fdd3a17852ad732583b688298cf6aa5a4ee7
drejon <drc.working@gmail.com>
2023-11-30 12:50:38 +0100

    Fix images path

--- src/consts/stack.js
+++ src/consts/stack.js
@@ -1,9 +1,9 @@
 export const stack = [
-  '../../public/images/react.svg',
-  '../../public/images/node.svg',
-  '../../public/images/express.svg',
-  '../../public/images/mysql.svg',
-  '../../public/images/vite.svg',
-  '../../public/images/git.svg',
-  '../../public/images/mocha.svg',
+  '../../images/react.svg',
+  '../../images/node.svg',
+  '../../images/express.svg',
+  '../../images/mysql.svg',
+  '../../images/vite.svg',
+  '../../images/git.svg',
+  '../../images/mocha.svg',
 ]
