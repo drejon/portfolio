772a55f6f26b6fcbd7b4db1cf40b611790ec7ea6
drejon <drc.working@gmail.com>
2023-12-04 14:25:42 +0100

    Testing Router

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path';
 
-const createAlias = (folder) => ({
-  [`nested-${folder}`]: path.resolve(__dirname, `${folder}/src`),
-});
+// const createAlias = (folder) => ({
+//   [`nested-${folder}`]: path.resolve(__dirname, `${folder}/src`),
+// });
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()]
   // resolve: {
   //   alias: {
   //     ...createAlias('battleshipV2'),
